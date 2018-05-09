d3188c2ed7ad8dba342243163974fc0f5f9df0cf
Eduardo <eduguia2009@gmail.com>
2018-05-10 00:19:49 +0100

    add slider

---
+++ js/main-js.js
@@ -0,0 +1,32 @@
+var contador = 1;
+
+$(document).ready(function () {
+	/* MENU */
+	$(".block-icon").on("click", function () {
+		if (contador == 1) {
+			$('.nav-mobile-group').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('.nav-mobile-group').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+	$(".item-group").on("click", function () {
+		if (contador != 1) {
+			contador = 1;
+			$('.nav-mobile-group').animate({
+				left: '-100%'
+			});
+		}
+	});
+	/*FIN MENU*/
+
+	$('.flexslider').flexslider({
+		animation: "slide"
+	});
+});
