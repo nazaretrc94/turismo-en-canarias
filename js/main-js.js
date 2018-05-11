b67905e9271f65768c8293f0d7f197da17a38a90
nazaretrc94 <39070042+nazaretrc94@users.noreply.github.com>
2018-05-11 10:05:29 +0100

    add

--- js/main-js.js
+++ js/main-js.js
@@ -1,32 +1,39 @@
 var contador = 1;
 
 $(document).ready(function () {
-	/* MENU */
-	$(".block-icon").on("click", function () {
-		if (contador == 1) {
-			$('.nav-mobile-group').animate({
-				left: '0'
-			});
-			contador = 0;
-		} else {
-			contador = 1;
-			$('.nav-mobile-group').animate({
-				left: '-100%'
-			});
-		}
-	});
+    /* MENU */
+    $(".block-icon").on("click", function () {
+        if (contador == 1) {
+            $('.nav-mobile-group').animate({
+                left: '0'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('.nav-mobile-group').animate({
+                left: '-100%'
+            });
+        }
+    });
 
-	$(".item-group").on("click", function () {
-		if (contador != 1) {
-			contador = 1;
-			$('.nav-mobile-group').animate({
-				left: '-100%'
-			});
-		}
-	});
-	/*FIN MENU*/
+    $(".item-group").on("click", function () {
+        if (contador != 1) {
+            contador = 1;
+            $('.nav-mobile-group').animate({
+                left: '-100%'
+            });
+        }
+    });
+    /*FIN MENU*/
 
-	$('.flexslider').flexslider({
-		animation: "slide"
-	});
+    $('.flexslider').flexslider({
+        animation: "slide"
+    });
+
+    $(".volver").on("click", function () {
+        var posicion = $(".section1").offset().top;
+        $("html, body").animate({
+            scrollTop: posicion
+        }, 2000);
+    });
 });
